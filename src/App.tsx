96502672c717750ec3e6c78d679076149a57b844
Rajgokul80565 <74341639+Rajgokul80565@users.noreply.github.com>
2024-02-08 22:59:11 +0530

    UI completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpense from './components/IncomeExpense';
+import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <Header />
+      <div className="container">
+          <Balance />
+          <IncomeExpense/>
+          <TransactionList/>
+          <AddTransaction/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    </div>
+   
+ 
   )
 }
 
---
+++ src/components/index.tsx
@@ -0,0 +1,5 @@
+export { default as Balance} from "./Balance";
+export {default as Header} from "./Header";
+export {default as IncomeExpense} from "./IncomeExpense";
+export {default as TransactionList} from "./TransactionList";
+export {default as AddTransaction } from "./AddTransaction";
